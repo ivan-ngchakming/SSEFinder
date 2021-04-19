68705514b80b109deb5bd88015a5d23b7934438a
ivan0313 <ivan0313@connect.hku.hk>
2021-04-19 17:04:08 +0800

    SSEF-9 Configured urls and basic view functions

--- SSEFinder/app/urls.py
+++ SSEFinder/app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('cases', views.cases, name='cases'),
+    path('events', views.events, name='events'),
+    path('cases/<int:id>', views.case, name='case'),
+    path('events/<int:id>', views.event, name='event'),
 ]
--- SSEFinder/app/views.py
+++ SSEFinder/app/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html", {})
+
+
+def login(request):
+    return HttpResponse("Login")
+
+
+def cases(request):
+    return HttpResponse("Cases")
+
+
+def events(request):
+    return HttpResponse("Superspreading Events")
+
+
+def case(request, id):
+    return HttpResponse(f"Case {id}")
+
+
+def event(request, id):
+    return HttpResponse(f"Event {id}")
