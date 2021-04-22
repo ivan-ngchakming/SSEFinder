e6dabc47ff5a2b3e7286d4e866cf560d952fffe2
ivan0313 <ivan0313@connect.hku.hk>
2021-04-22 18:51:33 +0800

    SSEF-19 Created Case Model (SSEF-35), dummy case data (SSEF-37) and a minimal index.html page (SSEF-36)

--- SSEFinder/app/views.py
+++ SSEFinder/app/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Case
 
 
 def index(request):
-    return render(request, "index.html", {})
+    # Create dummy data
+    # from faker import Faker
+    # fake = Faker()
+    # for i in range(10):
+    #     new_case = Case()
+    #     new_case.case_number = i
+    #     new_case.person_name = fake.name()
+    #     new_case.identity_document_number = fake.isbn10()
+    #     new_case.date_of_birth = fake.date_object()
+    #     new_case.onset_date = fake.date_object()
+    #     new_case.date_confirmed = fake.date_object()
+    #     new_case.save()
+
+    cases = Case.objects.all()
+
+    context = {
+        'cases': cases,
+    }
+
+    return render(request, "index.html", context)
 
 
 def login(request):
