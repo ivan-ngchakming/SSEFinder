f8a9679b6ba84b746e57038e98083b5640607dc8
ivan0313 <ivan0313@connect.hku.hk>
2021-04-26 18:38:07 +0800

    SSEF-20 Modified Event model and created Classification model. Modified views and templates to reflect changes made in models.py updated .gitignore

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import requests
 from django.db import models
 
 
@@ -14,20 +15,58 @@ class Case(models.Model):
 
 
 class Event(models.Model):
-    classification_choices = [
-    ('Infected', 'Infected'),
-    ('Infector', 'Infector'),
-    ('Both', 'Both'),
-    ]
-    case_number_name = models.ForeignKey(Case, on_delete=models.CASCADE)
-    venue_name = models.CharField(max_length = 100)
-    venue_location = models.CharField(max_length = 100)
-    address = models.CharField(max_length = 300, null=True, blank=True)
-    x_coord = models.FloatField(null=True, blank=True)
-    y_coord = models.FloatField(null=True, blank=True)
+    venue_name = models.CharField(max_length=100)
+    venue_location = models.CharField(max_length=100)
+    address = models.CharField(max_length=300, null=True, blank=True)  # Auto updated via api
+    x_coord = models.FloatField(null=True, blank=True)  # Auto updated via api
+    y_coord = models.FloatField(null=True, blank=True)  # Auto updated via api
     date_of_event = models.DateField()
-    description = models.CharField(max_length = 200)
-    classification = models.CharField(max_length=10, choices = classification_choices, default='Infected')
+    description = models.CharField(max_length=200)
+
+    def update_geodata(self):
+        """Connect to API and update x coord, y coord and address"""
+        query = {
+            "q": self.venue_location
+        }
+
+        response = requests.get(
+            "https://geodata.gov.hk/gs/api/v1.0.0/locationSearch?q=",
+            params=query,
+        ).json()
+
+        self.x_coord = response[0]['x']
+        self.y_coord = response[0]['y']
+        self.address = response[0]['addressEN']
+
+    def get_classification(self, case_number):
+        classifications = self.classification_set.all()
+        classification = [c for c in classifications if c.case.case_number == case_number]
+
+        if len(classification) > 1:
+            raise IndexError("Something has gone wrong")
+        else:
+            classification = classification[0]
+
+        result = []
+        if classification.infector:
+            result.append("Infector")
+        if classification.infected:
+            result.append("Infected")
+
+        return result
+
+    def get_classification_str(self, case_number):
+        return ", ".join(self.get_classification(case_number))
 
     def __str__(self):
         return self.venue_name
+
+
+class Classification(models.Model):
+    infected = models.BooleanField()
+    infector = models.BooleanField()
+    case = models.ForeignKey(Case, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.case
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Event, Case
+from .models import Event, Case, Classification
 
 
 def index(request):
@@ -41,32 +41,56 @@ def events(request):
 
 
 def case(request, id):
-    # test id
-    # id = 1234
+    # Create Dummy Event data
+    # locations = [
+    #     ("Kam Lok Hin Chicken and Fish Pot", "Conwell Mansion"),
+    #     ("Dynasty II", "The Dynasty Club"),
+    #     ("Hong Kong Cultural Centre Administration Building", "Hong Kong Cultural Centre Administration Building"),
+    #     ("The Flying Frenchman", "The Flying Frenchman"),
+    # ]
+    # from faker import Faker
+    # fake = Faker()
+    # for location in locations:
+    #     new_event = Event()
+    #     new_event.venue_name = location[0]
+    #     new_event.venue_location = location[1]
+    #     new_event.date_of_event = fake.date_object()
+    #     new_event.description = fake.text(200)
+    #     new_event.save()
+    #
+    # cases = Case.objects.all()
+    # events = Event.objects.all()
+    # for i, case in enumerate(cases):
+    #     for j, event in enumerate(events):
+    #         if i == j:
+    #             new_class = Classification(
+    #                 infected=True,
+    #                 infector=True,
+    #                 case=case,
+    #                 event=event
+    #             )
+    #             new_class.save()
+    #         if i > j:
+    #             new_class = Classification(
+    #                 infected=True,
+    #                 infector=False,
+    #                 case=case,
+    #                 event=event
+    #             )
+    #             new_class.save()
+    # Dummy data entry end
+
     case = Case.objects.get(pk=id)
+    all_classifications = case.classification_set.all()
+    events = [classification.event for classification in all_classifications]
+    classifications = [event.get_classification_str(case.case_number) for event in events]
+    events = list(zip(events, classifications))
 
-    # Retrieve all events with matching id
-    event_location_only = Event.objects.filter(case_number_name=id).values('venue_location')
-
-    # Connect to API and update x coord, y coord and address
-    for i in event_location_only:
-        venue_loc = i['venue_location']
-        query = {
-            "q": venue_loc
-        }
-        response = requests.get("https://geodata.gov.hk/gs/api/v1.0.0/locationSearch?q=", params=query).json()
-        x_val = response[0]['x']
-        y_val = response[0]['y']
-        address_val = response[0]['addressEN']
-        Event.objects.filter(venue_location=venue_loc).update(address=address_val)
-        Event.objects.filter(venue_location=venue_loc).update(x_coord=x_val)
-        Event.objects.filter(venue_location=venue_loc).update(y_coord=y_val)
-
-    events = Event.objects.filter(case_number_name=id)
     context = {
         'case': case,
-        'events': events
+        'events': events,
     }
+
     return render(request, 'case_expand.html', context)
 
 
